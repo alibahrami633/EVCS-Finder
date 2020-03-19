0d40944b376e442fcb938ed7402709c5b97273f6
Aliagho <ali.bahrami633@yahoo.com>
2020-03-19 15:48:53 +1100

    some small changes on the style

--- assets/script/map.js
+++ assets/script/map.js
@@ -1,16 +1,19 @@
 var map;
-var myLocation = { lat: -37.877721, lng: 145.045580 };
+
+// lat and lng values will be altered with dynamic variables returned from the selected station in the result section "#search-results"
+// or it can be directly received as an object with "lat" and "lng" and "zoom" properties.
+var selectedLocation = { lat: -37.877721, lng: 145.045580 };
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: myLocation,
+        center: selectedLocation,
         zoom: 15
     });
 
     // creates the marker on the map
     var marker = new google.maps.Marker({
-        position: myLocation,
+        position: selectedLocation,
         map: map,
-        title: 'EVS Finder'
+        title: 'EVCS Finder'
     });
 }
